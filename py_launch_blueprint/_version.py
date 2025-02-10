55339946e615ab97308532c943821d2b23819a1c
Adane Moges <adanemoges6@gmail.com>
2025-02-10 12:37:51 +0300

    Bump version to 0.1.dev101 in _version.py; clean up pyproject.toml

--- py_launch_blueprint/_version.py
+++ py_launch_blueprint/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev100'
-__version_tuple__ = version_tuple = (0, 1, 'dev100')
+__version__ = version = '0.1.dev101'
+__version_tuple__ = version_tuple = (0, 1, 'dev101')
